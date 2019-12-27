829fa13ef4148ed5f189520adec89a4a25e7b325
HeideQueen <lucyfabz@gmail.com>
2019-12-27 03:51:46 -0600

    Change sound clips source

--- src/App.utils.js
+++ src/App.utils.js
@@ -2,47 +2,47 @@ const sounds = [
   {
     id: 'boom',
     letter: 'Q',
-    source: '../../sounds/boom.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
   },
   {
     id: 'clap',
     letter: 'W',
-    source: '../../sounds/clap.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
   },
   {
     id: 'hihat',
     letter: 'E',
-    source: '../../sounds/hihat.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
   },
   {
     id: 'kick',
     letter: 'A',
-    source: '../../sounds/kick.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
   },
   {
     id: 'openhat',
     letter: 'S',
-    source: '../../sounds/openhat.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
   },
   {
     id: 'ride',
     letter: 'D',
-    source: '../../sounds/ride.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
   },
   {
     id: 'snare',
     letter: 'Z',
-    source: '../../sounds/snare.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
   },
   {
     id: 'tink',
     letter: 'X',
-    source: '../../sounds/tink.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
   },
   {
     id: 'tom',
     letter: 'C',
-    source: '../../sounds/tom.wav'
+    source: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
   }
 ];
 
--- src/components/drum-pad/drum-pad.component.jsx
+++ src/components/drum-pad/drum-pad.component.jsx
@@ -3,15 +3,23 @@ import React from 'react';
 const DrumPad = ({ id, letter, source }) => {
   const audioPlay = () => {
     const clip = document.getElementById(letter);
+    clip.currentTime = 0;
     clip.play();
   };
 
-  console.log(source);
+  const onKeyPressed = event => {
+    console.log(event.key);
+  };
 
   return (
-    <div className='drum-pad' id={id} onClick={audioPlay}>
+    <div
+      className="drum-pad"
+      id={id}
+      onClick={audioPlay}
+      onKeyDown={event => onKeyPressed(event)}
+    >
       {letter}
-      <audio src={source} className='clip' id={letter}></audio>
+      <audio src={source} className="clip" id={letter} />
     </div>
   );
 };
